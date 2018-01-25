8b74f822292116057056d32aafaec5b7e908a2f6
chengliangshi <chengliangshi@whu.edu.cn>
2018-01-24 18:50:39 -0800

    save

--- pgintegrity--0.0.1.sql
+++ pgintegrity--0.0.1.sql
@@ -1,7 +1,4 @@
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
 \echo Use "CREATE EXTENSION pgintegrity" to load this file. \quit
 
-CREATE FUNCTION pg_integrity()
-RETURNS trigger
-AS '$libdir/pgintegrity'
-LANGUAGE C;
+CREATE FUNCTION pg_integrity() RETURNS trigger AS '$libdir/pgintegrity' LANGUAGE C;
